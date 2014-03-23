5709c277534af3a1f203dbb50a911376efcbfb71
Tony Florida <raddfood@gmail.com>
2014-03-23 16:13:02 -0700

    Added additional functionality such as getting the frictlist and removing a frict

--- account.php
+++ account.php
@@ -52,6 +52,34 @@ function sign_in($email, $password, $db, $table)
 } //end sign_in()
 
 /*
+ * @brief Send all (not deleted) frict data to user
+ * @param uid the user id of the user adding the frict
+ * @param db the database object
+ * @retval -50 if invalid uid is discovered
+ * @retval on success, a table of the uer's fricts in which columns are separated by tabs and rows by new lines
+ */
+function get_frictlist($uid, $db)
+{
+   if($uid == null || $uid < 0)
+   {
+      return -50;
+   }
+   
+   //query database for provided email
+   $query="select frict.frict_id, frict.accepted, frict.frict_from_date, frict.frict_to_date, frict.frict_base, frict.notes, hookup.hu_first_name, hookup.hu_last_name, hookup.hu_gender from frict inner join hookup on frict.hu_id = hookup.hu_id where frict.uid=? AND frict.deleted=0;";
+   $sql=$db->prepare($query);
+   $sql->bind_param('i', $uid);
+   $sql->execute();
+   $sql->bind_result($frict_id, $accepted, $frict_from_date, $frict_to_date, $frict_base, $notes, $hu_first_name, $hu_last_name, $hu_gender);
+   echo "frictlist\n";
+   while($sql->fetch())
+   {
+      echo $frict_id."\t".$hu_first_name."\t".$hu_last_name."\t".$frict_base."\t".$accepted."\t".$frict_from_date."\t".$frict_to_date."\t".$notes."\t".$hu_gender."\n";
+   }
+   $sql->free_result();
+}
+
+/*
  * @brief Allows a user to sign up for an account
  * @param firstname the first name of the user up to 255 characters
  * @param lastname the first name of the user up to 255 characters
@@ -150,7 +178,11 @@ function pw_hash($password)
  * @param db the database object
  * @param table_hu the table name of the hookup table
  * @param table_frict the table name of the frict table
- * @retval TODO
+ * @retval -20 if an null or invalid uid
+ * @retval -21 if uid isn't found
+ * @retval -22 if the insert into the hookup table was not successful
+ * @retval -23 if the insert into the frict table was not successful
+ * @retval frict_id if the frict and hookup tables were updated successfully
  */
 function add_frict($uid, $firstname, $lastname, $gender, $base, $from, $to, $notes, $db, $table_user, $table_hu, $table_frict)
 {
@@ -228,9 +260,16 @@ function add_frict($uid, $firstname, $lastname, $gender, $base, $from, $to, $not
  * @param from the last occurrence of the frict
  * @param notes notes about the frict
  * @param db the database object
+ * @param table_user the table name of the user table
  * @param table_hu the table name of the hookup table
  * @param table_frict the table name of the frict table
- * @retval TODO
+ * @retval -30 if the uid or frict_id is null or invalid
+ * @retval -31 if the uid wasn't found
+ * @retval -32 if the frict_id wasn't found
+ * @retval -33 if the hu_id wasn't found
+ * @retval -34 if the update of the hookup table was not successful
+ * @retval -35 if the update of the frict table was not successful
+ * @retval frict_id if the update of the hookup and frict table was successful
  */
 function update_frict($uid, $frict_id, $firstname, $lastname, $gender, $base, $from, $to, $notes, $db, $table_user, $table_hu, $table_frict)
 {
@@ -264,7 +303,7 @@ function update_frict($uid, $frict_id, $firstname, $lastname, $gender, $base, $f
       return -31;
    }
    
-      //the uid was found and is unique so we can proceed with the insert
+   //the uid was found and is unique so we can proceed with the insert
    if($numrows_frict != 1)
    {
       //the frict_id was not found so return
@@ -287,10 +326,12 @@ function update_frict($uid, $frict_id, $firstname, $lastname, $gender, $base, $f
       return -33;
    }
 
+   $datetime = date("Y-m-d H:i:s");
+   
    //update hookup table
-   $query4="update ".$table_hu." set hu_first_name=?, hu_last_name=?, hu_gender=? where hu_id=?";
+   $query4="update ".$table_hu." set hu_first_name=?, hu_last_name=?, hu_gender=?, last_update=? where hu_id='".$hu_id."'";
    $sql4=$db->prepare($query4);
-   $sql4->bind_param('ssii', $firstname, $lastname, $gender, $hu_id);
+   $sql4->bind_param('ssis', $firstname, $lastname, $gender, $datetime);
    $sql4->execute();
    $sql4->store_result();
    $numrows4=$sql4->affected_rows;
@@ -304,9 +345,9 @@ function update_frict($uid, $frict_id, $firstname, $lastname, $gender, $base, $f
    } 
 
    //update frict table
-   $query5="update ".$table_frict." set frict_from_date=?, frict_to_date=?, frict_base=?, notes=? where frict_id=?";
+   $query5="update ".$table_frict." set frict_from_date=?, frict_to_date=?, frict_base=?, notes=?, last_update=? where frict_id='".$frict_id."'";
    $sql5=$db->prepare($query5);
-   $sql5->bind_param('ssisi', $from, $to, $base, $notes, $frict_id);
+   $sql5->bind_param('ssiss', $from, $to, $base, $notes, $datetime);
    $sql5->execute();
    $sql5->store_result();
    $numrows5=$sql5->affected_rows;
@@ -317,6 +358,79 @@ function update_frict($uid, $frict_id, $firstname, $lastname, $gender, $base, $f
    {
       //something went wrong when updating frict table
       return -35;
+   }
+   
+   return $frict_id;
+}
+
+/*
+ * @brief Allows a user to remove a frict to their list
+ * @param uid the user id of the user removing the frict
+ * @param frict_id the id of the frict
+ * @param db the database object
+ * @param table_user the table name of the user table
+ * @param table_frict the table name of the frict table
+ * @retval -40 if uid or frict_id was null or invalid
+ * @retval -41 if the uid wasn't found
+ * @retval -42 if the frict_id wasn't found
+ * @retval -43 if the update of the frict table wasn't successful
+ * @retval frict_id if the update of the frict table was successful
+ */
+function remove_frict($uid, $frict_id, $db, $table_user, $table_frict)
+{
+   if($uid == null || $uid < 0 || $frict_id == null || $frict_id < 0)
+   {
+      return -40;
+   }
+
+   //check that the uid exists in the db
+   $query="select * from ".$table_user." where uid=?";
+   $sql=$db->prepare($query);
+   $sql->bind_param('s', $uid);
+   $sql->execute();
+   $sql->store_result();
+   $numrows_user=$sql->num_rows;
+   $sql->free_result();
+   
+   //check that the frict_id exists in the db
+   $query2="select * from ".$table_frict." where frict_id=?";
+   $sql2=$db->prepare($query2);
+   $sql2->bind_param('s', $frict_id);
+   $sql2->execute();
+   $sql2->store_result();
+   $numrows_frict=$sql2->num_rows;
+   $sql2->free_result();
+
+   //the uid was found and is unique so we can proceed with the insert
+   if($numrows_user != 1)
+   {
+      //the uid was not found so return
+      return -41;
+   }
+   
+   //the uid was found and is unique so we can proceed with the insert
+   if($numrows_frict != 1)
+   {
+      //the frict_id was not found so return
+      return -42;
+   }
+
+   $datetime = date("Y-m-d H:i:s");
+
+   //update frict table
+   $query5="update ".$table_frict." set deleted=1, last_update=? where frict_id='".$frict_id."'";
+   $sql5=$db->prepare($query5);
+   $sql5->bind_param('s', $datetime);
+   $sql5->execute();
+   $sql5->store_result();
+   $numrows5=$sql5->affected_rows;
+   $sql5->free_result();
+   
+   //check result is TRUE meaning the update was successful
+   if($numrows5 != 1)
+   {
+      //something went wrong when updating frict table
+      return -43;
    }
    
    return $frict_id;
